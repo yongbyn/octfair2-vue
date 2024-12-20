1c8254d9cb99a0ca0d3156744f89f0a4e0694102
nyr6230 <nyr6230@gmail.com>
2024-12-20 15:56:07 +0900

    feat:#21 (구직자) 채용 공고 페이지

---
+++ src/views/Jobs/Post.vue
@@ -0,0 +1,11 @@
+<template>
+  <ContextBox>채용 공고</ContextBox>
+  <PostSearch />
+  <PostMain />
+</template>
+
+<script setup>
+const providedValue = ref();
+
+provide("providedValue", providedValue);
+</script>
