890d84ed8b78a0cdaa11391ecd9fa527313156cc
compactspace <younggoo7000@naver.com>
2024-12-20 09:18:29 +0900

    환경파일 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,52 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Login from "../views/Login/Login.vue";
-import DashBoard from "../views/DashBoard/DashBoard.vue";
-import Notice from "../views/Board/Notice.vue";
-import NoticeDetail from "../components/page/Board/Notice/NoticeDetail.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../views/Login/Login.vue';
+import DashBoard from '../views/DashBoard/DashBoard.vue';
+import Notice from '../views/Board/Notice.vue';
+import NoticeDetail from '../components/page/Board/Notice/NoticeDetail.vue';
+import Qna from '../views/Qna/QuesnAnsw.vue';
+import QnaDetailModal from '../components/page/Qna/QnaDetailModal.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "login",
+    path: '/',
+    name: 'login',
     component: Login,
   },
   {
-    path: "/vue",
-    name: "vue",
+    path: '/vue',
+    name: 'vue',
     component: DashBoard,
     children: [
       {
-        path: "board",
-        name: "board",
+        path: 'board',
+        name: 'board',
         children: [
           {
-            path: "notice.do",
-            name: "notice",
+            path: 'notice.do',
+            name: 'notice',
             component: Notice,
           },
           {
-            path: "notice.do/:idx",
-            name: "noticeDetail",
+            path: 'notice.do/:idx',
+            name: 'noticeDetail',
             component: NoticeDetail,
           },
           {
-            path: "notice.do/insert",
-            name: "noticeInsert",
+            path: 'notice.do/insert',
+            name: 'noticeInsert',
             component: NoticeDetail,
+          },
+
+          {
+            path: 'qna.do',
+            qna: 'qna',
+            component: Qna,
+          },
+
+          {
+            path: 'qnaInsert.do/insert',
+            qna: 'qnaInsert',
+            component: QnaDetailModal,
           },
         ],
       },
@@ -41,7 +55,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory("/"),
+  history: createWebHistory('/'),
   routes,
 });
 
