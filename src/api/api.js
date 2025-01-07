fb9225425a2de80f8aec119960503742c0e310c3
YongBin Kim <yongbyn@naver.com>
2025-01-07 20:34:09 +0900

    feat:#131 회원탈퇴 페이지 구현

--- src/api/api.js
+++ src/api/api.js
@@ -14,14 +14,9 @@ export const Post = {
   DeletePostDetail: "/prx/api/manage-hire/deleteHirePost.do",
 };
 
-export const  Withdraw={
-
-    WithdrawConfirm:"/prx/api/mypage/deleteUser.do"
-
+export const Withdraw = {
+  WithdrawConfirm: "/prx/api/mypage/deleteUser.do",
 };
-
-
-
 
 export const Scrap = {
   SearchScrapList: "/prx/api/jobs/readScrapList.do",
@@ -78,6 +73,8 @@ export const MyPage = {
   GetUserInfo: "/prx/api/mypage/userDetail.do",
   UpdateUserInfo: "/prx/api/mypage/updateUserInfo.do",
   UpdatePwd: "/prx/api/mypage/updatePw.do",
+  DeleteUser: "/prx/mypage/deleteUser.do",
+  DeleteUserBcrypt: "/prx/api/mypage/deleteUser.do",
 };
 
 export const Applicant = {
@@ -85,13 +82,13 @@ export const Applicant = {
   SearchApplicantDetail: "/prx/api/manage-user/applicantManageDetail.do",
   ResetApplicantPw: "/prx/api/manage-user/applicantPwReset.do",
   UpdateApplicantDetail: "/prx/api/manage-user/applicantInfoUpdate.do",
-  ApplicantManagerGetId:"/prx/api/manage-hire/applicant.do",
-  ApllicantList:"/prx/api/manage-hire/applicantList.do",
-  ApplicantStatusUpdate:"/prx/api/manage-hire/statusUpdate.do",
-  ApplicantResumeIndependent:"prx/api/apply/resumeDetail",
-  ApplicantcareerListIndependent:"/prx/api/apply/careerList",
-  ApplicantEducationListtIndependent:"/prx/api/apply/educationListt",
-  ApplicantskillListIndependent:"/prx/api/apply/skillList",
+  ApplicantManagerGetId: "/prx/api/manage-hire/applicant.do",
+  ApllicantList: "/prx/api/manage-hire/applicantList.do",
+  ApplicantStatusUpdate: "/prx/api/manage-hire/statusUpdate.do",
+  ApplicantResumeIndependent: "prx/api/apply/resumeDetail",
+  ApplicantcareerListIndependent: "/prx/api/apply/careerList",
+  ApplicantEducationListtIndependent: "/prx/api/apply/educationListt",
+  ApplicantskillListIndependent: "/prx/api/apply/skillList",
 };
 
 export const Faq = {
--- src/components/page/MyPage/WithdrawMain.vue
+++ src/components/page/MyPage/WithdrawMain.vue
@@ -1,5 +1,25 @@
-<template></template>
+<template>
+  <div class="withdraw-sentence-container">
+    사용하고 계신 아이디(세션ID)를 탈퇴할 경우 재사용 및 복구가 불가능합니다.
+    <br />
+    탈퇴 후 회원정보 및 개인형 서비스 이용기록은 모두 삭제됩니다.<br />
+    안내 사항을 모두 확인하였으며, 이에 동의합니다.<br />
+  </div>
+  <div class="withdraw-button-container">
+    <b-button @click="handlerUserDeleteModal">확인</b-button>
+  </div>
+  <WithdrawModal v-if="modalStore.modalState" />
+</template>
 
-<script setup></script>
+<script setup>
+import { useModalStore } from "../../../stores/modalState";
+import WithdrawModal from "./WithdrawModal.vue";
+
+const modalStore = useModalStore();
+
+const handlerUserDeleteModal = () => {
+  modalStore.setModalState();
+};
+</script>
 
 <style scoped></style>
