f90a8fba5590fc3387d8faafa88be439504d5750
itiswanthefull <itiswanthefull@gmail.com>
2025-01-08 00:38:26 +0900

    실수 수정

--- src/components/page/Apply/Resume/InnerPage/EduList.vue
+++ src/components/page/Apply/Resume/InnerPage/EduList.vue
@@ -5,28 +5,28 @@
   <template v-for="(item, key) in eduList?.payload" :key="key">
     <div class="edu_table">
       <div class="garo_wrapper_lr" style="grid-area: admDate">
-        <label class="resume_label" class="garo_wrapper_lr_l">입학연도:</label>
-        <textarea class="resume_textarea" class="garo_wrapper_lr_r" :value="item.admDate.slice(0, 7)" placeholder="입학연도" type="month" disabled />
+        <label class="resume_label">입학연도:</label>
+        <textarea class="resume_textarea" :value="item.admDate.slice(0, 7)" placeholder="입학연도" type="month" disabled />
       </div>
       <div class="garo_wrapper_lr" style="grid-area: eduLevel">
-        <label class="resume_label" class="garo_wrapper_lr_l">학력구분:</label>
-        <textarea class="resume_textarea" class="garo_wrapper_lr_r" :value="item.eduLevel" placeholder="학력구분" disabled />
+        <label class="resume_label">학력구분:</label>
+        <textarea class="resume_textarea" :value="item.eduLevel" placeholder="학력구분" disabled />
       </div>
       <div class="garo_wrapper_lr" style="grid-area: grdStatus">
-        <label class="resume_label" class="garo_wrapper_lr_l">졸업여부:</label>
-        <textarea class="resume_textarea" class="garo_wrapper_lr_r" :value="item.grdStatus" placeholder="졸업여부" disabled />
+        <label class="resume_label">졸업여부:</label>
+        <textarea class="resume_textarea" :value="item.grdStatus" placeholder="졸업여부" disabled />
       </div>
       <div class="garo_wrapper_lr" style="grid-area: grdDate">
-        <label class="resume_label" class="garo_wrapper_lr_l">졸업연도:</label>
-        <textarea class="resume_textarea" class="garo_wrapper_lr_r" :value="item.grdDate.slice(0, 7)" placeholder="졸업연도" type="month" disabled />
+        <label class="resume_label">졸업연도:</label>
+        <textarea class="resume_textarea" :value="item.grdDate.slice(0, 7)" placeholder="졸업연도" type="month" disabled />
       </div>
       <div class="garo_wrapper_lr" style="grid-area: schoolName">
-        <label class="resume_label" class="garo_wrapper_lr_l">학교명:</label>
-        <textarea class="resume_textarea" class="garo_wrapper_lr_r" :value="item.schoolName" placeholder="학교명" disabled />
+        <label class="resume_label">학교명:</label>
+        <textarea class="resume_textarea" :value="item.schoolName" placeholder="학교명" disabled />
       </div>
       <div class="garo_wrapper_lr" style="grid-area: major">
-        <label class="resume_label" class="garo_wrapper_lr_l">전공명:</label>
-        <textarea class="resume_textarea" class="garo_wrapper_lr_r" :value="item.major" placeholder="전공명" disabled />
+        <label class="resume_label">전공명:</label>
+        <textarea class="resume_textarea" :value="item.major" placeholder="전공명" disabled />
       </div>
       <div class="garo_wrapper_r" style="grid-area: button; display: flex; justify-content: right; align-items: center;">
         <CommonButton @click="handlerDeleteEduBtn({ resIdx: props.resume.resIdx, eduIdx: item.eduIdx })" v-if="props.isShow">삭제</CommonButton>
@@ -37,12 +37,12 @@
   <div>
     <div class="edu_table" v-if="isAddEdu && props.isShow">
       <div class="garo_wrapper_lr" style="grid-area: admDate">
-        <label class="resume_label" class="garo_wrapper_lr_l">입학연도:</label>
-        <input class="resume_input" class="garo_wrapper_lr_r" v-model=edu.admDate id="admDate" placeholder="입학연도" type="month"></input>
+        <label class="resume_label">입학연도:</label>
+        <input class="resume_input" v-model=edu.admDate id="admDate" placeholder="입학연도" type="month"></input>
       </div>
       <div class="garo_wrapper_lr" style="grid-area: eduLevel">
-        <label class="resume_label" class="garo_wrapper_lr_l">학력구분:</label>
-        <select class="resume_select" class="garo_wrapper_lr_r" v-model=edu.eduLevel id="eduLevel" placeholder="학력구분">
+        <label class="resume_label">학력구분:</label>
+        <select class="resume_select" v-model=edu.eduLevel id="eduLevel" placeholder="학력구분">
           <option value="고등학교">고등학교</option>
           <option value="대학교">대학교</option>
           <option value="대학원(석사)">대학원(석사)</option>
@@ -50,8 +50,8 @@
         </select>
       </div>
       <div class="garo_wrapper_lr" style="grid-area: grdStatus">
-        <label class="resume_label" class="garo_wrapper_lr_l">졸업여부:</label>
-        <select class="resume_select" class="garo_wrapper_lr_r" v-model=edu.grdStatus id="grdStatus" placeholder="졸업여부">
+        <label class="resume_label">졸업여부:</label>
+        <select class="resume_select" v-model=edu.grdStatus id="grdStatus" placeholder="졸업여부">
           <option value="졸업">졸업</option>
           <option value="재학">재학</option>
           <option value="휴학">휴학</option>
@@ -59,16 +59,16 @@
         </select>
       </div>
       <div class="garo_wrapper_lr" style="grid-area: grdDate">
-        <label class="resume_label" class="garo_wrapper_lr_l">졸업연도:</label>
-        <input class="resume_input" class="garo_wrapper_lr_r" v-model=edu.grdDate id="grdDate" placeholder="졸업연도" type="month"></input>
+        <label class="resume_label">졸업연도:</label>
+        <input class="resume_input" v-model=edu.grdDate id="grdDate" placeholder="졸업연도" type="month"></input>
       </div>
       <div class="garo_wrapper_lr" style="grid-area: schoolName">
-        <label class="resume_label" class="garo_wrapper_lr_l">학교명:</label>
-        <input class="resume_input" class="garo_wrapper_lr_r" v-model=edu.schoolName id="schoolName" placeholder="학교명"></input>
+        <label class="resume_label">학교명:</label>
+        <input class="resume_input" v-model=edu.schoolName id="schoolName" placeholder="학교명"></input>
       </div>
       <div class="garo_wrapper_lr" style="grid-area: major">
-        <label class="resume_label" class="garo_wrapper_lr_l">전공명:</label>
-        <input class="resume_input" class="garo_wrapper_lr_r" v-model=edu.major id="major" placeholder="전공명"></input>
+        <label class="resume_label">전공명:</label>
+        <input class="resume_input" v-model=edu.major id="major" placeholder="전공명"></input>
       </div>
       <div class="garo_wrapper_r" style="grid-area: button; display: flex; justify-content: right; align-items: center;">
         <CommonButton @click="handlerCreateEduBtn({ resIdx: props.resume.resIdx, edu: edu })">저장</CommonButton>
